2115ce26ca0f63da94fcb200f6c4adfd04857b33
hdwebvn <hongdanh.itnet@gmail.com>
2023-02-13 16:34:24 +0700

    D2-Test Rewrite URL

--- js/myController.js
+++ js/myController.js
@@ -1,12 +1,12 @@
 // Khai bao Angular App
 var app = angular.module("myApp", ["ngRoute"]);
-// app.config(function ($routeProvider, $locationProvider) {
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
+    // app.config(function ($routeProvider) {
     $routeProvider
         .when("/", { templateUrl: "./views/home.html", controller: "myControll" })
         .when("/bio", { templateUrl: "./views/detail-biography.html", controller: "myControll" })
-    // .otherwise({ redirectTo: '/' })
-    // $locationProvider.html5Mode(true);
+        .otherwise({ redirectTo: '/' })
+    $locationProvider.html5Mode(true);
 });
 
 app.controller('myControll', function ($scope, $http) {
